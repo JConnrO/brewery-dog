30e19db28206ff6dadf501d1c3ba9d77ac2ab21f
Sonika <sonikapoudyal5@gmail.com>
2021-05-28 19:40:17 -0400

    changes in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     $("#submit").on("click", function(){
-        var text = $("#brewsearch").val();
+        var text = $("#brew-search").val();
         console.log(text);
         //search history for the places in local storage
         localStorage.setItem("text", text);
