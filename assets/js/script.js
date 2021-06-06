41059e7500c1578dc5243711139a0f5e585813e2
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-06-06 17:05:04 -0400

    Fixed Revent History and updated some alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,22 +25,44 @@ function loadMapHistory() {
     //If not local storage create local storage
     if (localStorage.getItem("mapHistory") === null) {
         //Set up Array
+        mapHistory.push("Belford Brewing Company");
         localStorage.setItem("mapHistory", mapHistory);
         saveMapHistory();
     }
     return JSON.parse(localStorage.getItem("mapHistory"));
 }
+function renderMapHistory() {
+    mapHistory = loadMapHistory();
+    //console.log("Line 69" + mapHistory);
 
+    $("#brewery-history").empty();
+    var breweryListLength = mapHistory.length;
+
+    if (breweryListLength > 5) {
+        breweryListLength = 5;
+    }
+
+    for (var i = 0; i < breweryListLength; i++) {
+        index = mapHistory.length - i - 1;
+        var brewEl = document.createElement("p");
+        brewEl.innerText = mapHistory[index];
+        brewEl.classList = "brewery-name-history";
+        brewEl.setAttribute("id", "brewery-name-history");
+        $("#brewery-history").append(brewEl);
+    }
+    $(".brewery-name-history").click(function () {
+        $("#map").empty();
+        var breweryName = $(this).text();
+        mapHistory = loadMapHistory();
+        renderMapHistory();
+        renderBreweryDirections(breweryName);
+    });
+}
 function displayBreweries(breweries) {
-    console.log(breweries.length);
-
     $("#brewery-list").empty();
-    var titleEl = document.createElement("h4");
-    titleEl.textContent = "Click a Brewery:";
-    $("#brewery-list").append(titleEl);
     var breweryListLength = breweries.length;
-    if (breweryListLength > 10) {
-        breweryListLength = 10;
+    if (breweryListLength > 8) {
+        breweryListLength = 8;
     }
     for (var i = 0; i < breweryListLength; i++) {
         var brewery = breweries[i];
@@ -50,15 +72,6 @@ function displayBreweries(breweries) {
         brewEl.innerText = brewery.name;
         brewEl.classList = "brewery-name";
         brewEl.setAttribute("id", "brewery-name");
-
-        //function to make the font-size bigger of the brewery lists
-        $("#brewery-list").css("font-size", "20px");
-
-        //function to add cursor when clicked on list
-        $("#brewery-list").css("cursor", "pointer");
-
-        //brewEl.setAttribute("iframeSrc",)
-        //Onclick: Return BreweryName 
         $("#brewery-list").append(brewEl);
     }
     $(".brewery-name").click(function () {
@@ -71,45 +84,7 @@ function displayBreweries(breweries) {
         renderBreweryDirections(breweryName);
     });
 }
-function renderMapHistory() {
-    mapHistory = loadMapHistory();
-    //console.log("Line 69" + mapHistory);
 
-    $("#brewery-history").empty();
-    var titleEl = document.createElement("h4");
-    titleEl.textContent = "Search History:";
-    $("#brewery-history").append(titleEl);
-
-    var breweryListLength = mapHistory.length;
-
-    if (breweryListLength > 5) {
-        breweryListLength = 5;
-    }
-
-    for (var i = 0; i < breweryListLength; i++) {
-        index = mapHistory.length - i - 1;
-        var brewEl = document.createElement("p");
-        brewEl.innerText = mapHistory[index];
-        brewEl.classList = "brewery-name-history col";
-        brewEl.setAttribute("id", "brewery-name-history");
-
-        //function to make the font-size bigger of the brewery lists
-        $("#brewery-history").css("font-size", "14px");
-
-        // //function to add cursor when clicked on list
-        $("#brewery-history").css("cursor", "pointer");
-
-        // //Onclick: Return BreweryName 
-        $("#brewery-history").append(brewEl);
-    }
-    $(".brewery-name-history").click(function () {
-        $("#map").empty();
-        var breweryName = $(this).text();
-        mapHistory = loadMapHistory();
-        renderMapHistory();
-        renderBreweryDirections(breweryName);
-    });
-}
 
 function renderBreweryDirections(breweryName) {
     var mapsKey = "AIzaSyD_XHESF0-cQkfMSd2HgoAIeWN6PPRHh0Q";
@@ -125,3 +100,11 @@ function renderBreweryDirections(breweryName) {
     iframeEl.setAttribute("src", iframeSrc);
     mapHTML.append(iframeEl);
 };
+function renderApp(){
+
+    renderMapHistory();
+    renderBreweryDirections(mapHistory[mapHistory.length-1]);
+
+}
+renderApp();
+
